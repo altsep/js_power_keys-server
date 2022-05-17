3752c7ec26157bf415b81bf36c93a89fd8dba466
l <mx24xnpi4@mozmail.com>
2022-05-17 16:36:24 +0300

    ci: make app consider process.env when setting port

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const app = express();
 app.use(express.json());
 app.use(express.text());
 
-app.set('port', 3001);
+app.set('port', process.env.PORT || 3001);
+app.set('json spaces', 2);
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
