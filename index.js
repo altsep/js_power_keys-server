9a90a691994ef77d1de3ac9f9dee7f2c2c22a2fc
l <mx24xnpi4@mozmail.com>
2022-07-16 23:39:08 +0300

    feat: respond with search results instead of a strict match

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const axios = require('axios').default;
 const fs = require('fs');
-// const zlib = require('zlib');
 const geoip = require('geoip-lite');
 
 const app = express();
@@ -58,62 +57,62 @@ app.get('/api/steamapps', (req, res, next) => {
   });
 });
 
-// app.get('/api/steamapps/:name', (req, res, next) => {
-//   const { name } = req.params;
-//   fs.readFile('steamapps', 'base64', (err, data) => {
-//     if (err) {
-//       next(err);
-//     } else {
-//       const apps = JSON.parse(data)
-//         .filter((a) => a.name.toLowerCase().includes(name.toLowerCase()))
-//         .sort((a, b) => a.name.localeCompare(b.name));
-//       apps.length > 1 ? res.send(apps) : res.send(apps[0]);
-//     }
-//   });
-// });
-
-// const getSteamApps = async () => {
-//   try {
-//     const url = 'http://api.steampowered.com/ISteamApps/GetAppList/v2/';
-//     const { data } = await axios.get(url);
-//     let writeStream = fs.createWriteStream('steamapps');
-//     writeStream.write(JSON.stringify(data.applist));
-//     writeStream.end();
-//   } catch (err) {
-//     if (err) console.log(err);
-//   }
-// };
-
-// /* Get steam appid and name list on script load */
-// getSteamApps();
-
-// const msInADay = 86400000;
-
-// setInterval(() => {
-//   getSteamApps();
-// }, msInADay);
-
 app.get('/api/gogproducts/:name', async (req, res, next) => {
   const { name } = req.params;
-  const region = getRegion(req);
   const API = 'https://www.gogdb.org/';
   const url = new URL('products?search=' + name, API);
   try {
     const { data } = await axios.get(url.href);
     const nameHtml = name.replace("'", '&#39;');
     const regex = new RegExp(
-      `<a href="/product/\\d+" class="[a-z]+">(\n|\\s)+${nameHtml}(\n|\\s)+</a>`
+      `<a href="/product/\\d+" class="[a-z]+">(\n|\\s)+${nameHtml}.*(\n|\\s)+</a>`,
+      'ig'
     );
     const parsed = data.match(regex);
-    const id = parsed[0].match(/(?<=\/product\/)\d+/g);
-    const prices = await axios.get(
-      `https://api.gog.com/products/${id[0]}/prices?countryCode=${region}`
-    );
-    const r = prices.data._embedded.prices[0];
-    res.send(r);
+    console.log(parsed);
+    if (!parsed) {
+      throw new Error('Not found');
+    }
+    const items = parsed.map((item) => ({
+      id: item.match(/(?<=\/product\/)\d+/)[0],
+      name: item
+        .match(new RegExp(`(?<=\n\\s+)${nameHtml}.*`, 'i'))[0]
+        .replace('&#39;', "'"),
+    }));
+    console.log(items);
+    res.send(items);
   } catch (err) {
+    res.status(404).send({ message: err, serviceName: 'gog' });
     next(err);
-    res.status(500).send({ error: err.toString() });
+  }
+});
+
+app.get('/api/gogitemprice/:id', async (req, res, next) => {
+  const { id } = req.params;
+  const region = getRegion(req);
+  try {
+    const prices = await axios.get(
+      `https://api.gog.com/products/${id}/prices?countryCode=${region}`
+    );
+    const {
+      currency: { code },
+      basePrice,
+      finalPrice,
+    } = prices.data._embedded.prices[0];
+    const slicePrice = (price) =>
+      price[0] === '0' ? '0' : price.slice(0, price.length - 6);
+    res.send({
+      code,
+      basePrice: slicePrice(basePrice),
+      finalPrice: slicePrice(finalPrice),
+    });
+  } catch (err) {
+    if (err) {
+      const { data, status } = err.response;
+      const { message } = data;
+      res.status(status).send({ message });
+      next(err);
+    }
   }
 });
 
