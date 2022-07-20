fb1405121353d95ee8034eb7659b75bc5114f063
l <mx24xnpi4@mozmail.com>
2022-07-20 20:09:32 +0300

    feat: change the api for gog item and take price and image urls from there

--- services/gogitem.js
+++ services/gogitem.js
@@ -5,9 +5,26 @@ app.get('/api/gogitem/:id', async (req, res, next) => {
   const { id } = req.params;
   const { region, locale } = getRegion(req);
   try {
-    const prices = await axios.get(
-      `https://api.gog.com/products/${id}/prices?countryCode=${region}`
-    );
+    const item = await axios.get(`https://api.gog.com/v2/games/${id}`);
+    const {
+      inDevelopment: { active: inDevelopment },
+      _embedded: {
+        product: {
+          title: name,
+          globalReleaseDate,
+          gogReleaseDate,
+          _links: {
+            image: { href: img, formatters },
+            prices: { href: pricesUrl },
+          },
+          // productType,
+        },
+      },
+      _links: {
+        store: { href: productUrl },
+      },
+    } = item.data;
+    const prices = await axios.get(pricesUrl.replace('{country}', region));
     const {
       currency: { code: currencyCode },
       basePrice,
@@ -18,24 +35,18 @@ app.get('/api/gogitem/:id', async (req, res, next) => {
         .split(/\s(?=[a-z])/i)[0]
         .replace(/(?=\d{2}$)/, '.')
         .replace(/\.00$/, '');
-    const {
-      data: {
-        title: name,
-        links: { product_card: productUrl },
-        in_development: { active, until },
-        release_date,
-      },
-    } = await axios.get(`https://api.gog.com/products/${id}`);
+        console.log('requested gog item');
     res.send({
       name,
       currencyCode,
       basePrice: slicePrice(basePrice),
       finalPrice: slicePrice(finalPrice),
       productUrl,
-      status: basePrice[0] === '0' ? 'free' : active && 'coming soon',
-      releaseDate: until
-        ? formatDate(until, locale)
-        : release_date && formatDate(release_date, locale),
+      status: basePrice[0] === '0' ? 'free' : inDevelopment && 'coming soon',
+      releaseDate: gogReleaseDate
+        ? formatDate(gogReleaseDate, locale)
+        : formatDate(globalReleaseDate, locale),
+      headerImg: img.replace('{formatter}', formatters[4]),
     });
   } catch (err) {
     if (err.response) {
